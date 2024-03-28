a5f840cb89f85649a684ab3b49f8988ce11ea4f5
betodata <43320272+betodata@users.noreply.github.com>
2024-03-28 16:41:44 -0600

    Update template_example.js

--- IAT/template_example.js
+++ IAT/template_example.js
@@ -3,35 +3,39 @@ define(['pipAPI', 'https://betodata.github.io/IAT/template_main.js'], function(A
 
 	return iatExtension({
 		category1 : {
-			name : 'Democracia', //Will appear in the data.
+			name : 'Novorossiya', //Will appear in the data.
 			title : {
-				media: {word : 'Democracia'}, //Name of the category presented in the task.
+				media: {word : 'Novorossiya'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    			{word : 'Voto'}, 
-    			{word : 'Participación'}, 
-    			{word : 'Libre Expresión'}, 
-    			{word : 'Igualdad'},
-			{word : 'Tolerancia'}
+    			{word : 'Novorossiya'}, 
+    			{word : 'Vladimir Putin'}, 
+    			{word : 'Maria Zakharova'}, 
+    			{word : 'Sergei Lavrov'},
+			{word : 'Dmitry Kiselyov'},
+			{word : 'Vladimir Solovyov'},
+			{word : 'Special operation'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'Autoritarismo', //Will appear in the data.
+			name : 'Democracy', //Will appear in the data.
 			title : {
-				media : {word : 'Autoritarismo'}, //Name of the category presented in the task.
+				media : {word : 'Democracy'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    	{word : 'Dominación'}, 
-    			{word : 'Militarismo'}, 
-    			{word : 'Silenciar'}, 
-    			{word : 'Imposición'}, 
-    			{word : 'Represión'}				], 
+    		    	{word : 'Democracy'}, 
+    			{word : 'Human rights'}, 
+    			{word : 'Alexei Navalny'}, 
+    			{word : 'Boris Nemtsov'},
+			{word : 'Yabloko'},
+			{word : 'Mikhail Gorbachev'},
+    			{word : 'Mikhail Khodorkovsky'}				], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
